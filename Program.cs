039334bfd6712f2d776c984879ef1f701a58ac02
SpairowMichael <evv79767@gmail.com>
2024-07-04 15:18:17 +0300

    Program
    C# file

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿namespace learn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            float percent = 0.017f;
+            float money;
+            float months;
+
+            int monthCount = 0;
+
+            Console.WriteLine("Bank. Give money and you collect more. Percentage is 1.7%");
+
+            Console.Write("How much you have money: ");
+            money = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("How long you hold money in bank(in months): ");
+            months = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("1 - Simple interest");
+            Console.WriteLine("2 - Compound interest");
+            Console.WriteLine("What is the operation you want to complete:");
+            string choiceOfOperation = Console.ReadLine();
+
+            if (choiceOfOperation == "1")
+            {
+
+                Console.WriteLine($"Money before: {money}");
+
+                for (float i = months; i > 0; i--)
+                {
+                    float profit = money * percent;
+                    money += Convert.ToInt32(profit);
+
+                    Console.WriteLine($"{++monthCount} month: {money}");
+                }
+
+            }
+            else if (choiceOfOperation == "2")
+            {
+
+                Console.WriteLine($"Money before: {money}");
+
+                for (float i = months; i > 0; i--)
+                {
+                    money += money * percent;
+                    Console.WriteLine($"{++monthCount} month: {money}");
+                }
+
+            }
+            else
+            {
+
+                Console.WriteLine("Incorrect operation! Please, repeat the request");
+
+            }
+
+
+            Console.WriteLine($"Money after {months} months: {money}");
+
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
